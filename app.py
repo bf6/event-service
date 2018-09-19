b8e3567a5780bd3b216b1feefc7a7d07ec7ca234
Brian Fernandez <bfernandez@aaptiv.com>
2018-09-19 13:05:12 -0400

    Make it work

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
+import os
+import shutil
+import uuid
+
 import falcon
+import requests
+from InstagramAPI import InstagramAPI
+
+insta = InstagramAPI(os.environ.get('INSTAGRAM_USER'), os.environ.get('INSTAGRAM_PASS'))
+insta.login()
 
 class EventResource:
     def on_post(self, req, resp):
         try:
             # Get first hashtag
             hashtag = None
-            message_body = req.media['event']['text']
+            message_body = req.media['event']['message']['text']
             for word in message_body.split(' '):
                 if word.startswith('#'):
                     hashtag = word 
@@ -15,15 +24,20 @@ class EventResource:
 
             # Get image url
             image_url = None
-            if 'attachments' in req.media['event']:
-                image_url = req.media['event']['attachments'][0]['image_url']
-            else if 'files' in req.media['event']:
-                image_url = req.media['event']['files'][0]['url_private']
+            if 'attachments' in req.media['event']['message']:
+                image_url = req.media['event']['message']['attachments'][0]['image_url']
+            elif 'files' in req.media['event']['message']:
+                image_url = req.media['event']['message']['files'][0]['url_private']
             if image_url is None:
                 return
-
-            # TODO: Post to instagram, internal social feed, facebook, twitter, etc
-            print('hashtag: ', hashtag, 'image: ', image_url)
+            import pdb; pdb.set_trace()
+            r = requests.get(image_url, stream=True, headers={'Authorization': 'Bearer {}'.format(os.environ.get('SLACK_ACCESS_TOKEN'))})
+            if r.status_code == 200:
+                path = '{}'.format(uuid.uuid4().hex)
+                with open(path, 'wb') as f:
+                    shutil.copyfileobj(r.raw, f)
+                insta.uploadPhoto(path, caption=hashtag)
+                os.remove(path)
         except:
             pass
             
